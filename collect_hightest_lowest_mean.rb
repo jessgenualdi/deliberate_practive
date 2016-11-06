012bf8dc9fd575b9f405b7593177e61af2efbab7
Jessica Genualdi <jessi.genualdi@fcb.com>
2016-11-06 14:34:16 -0600

    collect highest and lowest mean exercise

--- collect_hightest_lowest_mean.rb
+++ collect_hightest_lowest_mean.rb
@@ -1,4 +1,4 @@
-array_of_array = [[3, 99, 88, 40, 6, 12], [17, 10, 31, 66, 2, 28], [39, 79, 1, 42, 88, 45]]
+array_of_array = [[3, 99, 88, 40, 6, 12], [17, 10, 31, 66, 2, 28], [39, 79, 1, 42, 88, 45], [67, 3, 33, 19, 27, 50], [53, 86, 33, 20, 41, 15]]
 new_array = []
 
 array_of_array.each do |array|
@@ -9,6 +9,23 @@ array_of_array.each do |array|
   new_array << sum
 end
 p new_array
+
+lowest_number = 1000
+new_array.each do |number|
+  if number < lowest_number
+    lowest_number = number
+  end
+end 
+puts "The lowest number is " + lowest_number.to_s
+
+
+highest_number = 0
+new_array.each do |number|
+  if number > highest_number
+    highest_number = number
+  end
+end 
+puts "The highest number is " + highest_number.to_s
 
 
 
